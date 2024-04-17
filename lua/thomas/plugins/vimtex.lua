549651c969fde8e908d30c9b810b6385c7c2838c
Thomas <thomasbandy23@gmail.com>
2024-04-17 16:56:45 +1000

    Latex Configs

--- lua/thomas/plugins/vimtex.lua
+++ lua/thomas/plugins/vimtex.lua
@@ -1,3 +1,31 @@
 return {
-	{ "lervag/vimtex" },
+	{
+		"lervag/vimtex",
+
+		-- Configuring LaTeX files with specific settings
+		config = function()
+			-- Set up the text wrapping and tab settings for LaTeX files
+			vim.cmd([[
+                augroup latex_settings
+                    autocmd!
+                    autocmd FileType tex setlocal textwidth=80
+                    autocmd FileType tex setlocal wrap
+                    autocmd FileType tex setlocal linebreak
+                augroup END
+            ]])
+
+			-- Setting tab behavior for better formatting control
+			vim.bo.tabstop = 4
+			vim.bo.shiftwidth = 4
+			vim.bo.expandtab = true -- Converts tabs to spaces
+
+			-- Optionally, to ensure these settings are correctly applied to tex files, add:
+			vim.cmd([[
+                augroup latex_tab_settings
+                    autocmd!
+                    autocmd FileType tex setlocal tabstop=4 shiftwidth=4 expandtab
+                augroup END
+            ]])
+		end,
+	},
 }
