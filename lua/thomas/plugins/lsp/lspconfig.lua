230f7facbc893ba174e25d461d7fa99df64a378c
Thomas <thomasbandy23@gmail.com>
2024-05-22 11:13:05 +1000

    Fixed Java LSP

--- lua/thomas/plugins/lsp/lspconfig.lua
+++ lua/thomas/plugins/lsp/lspconfig.lua
@@ -180,14 +180,45 @@ return {
 				})
 			end,
 
-			-- Configuration for Java (jdtls)
 			["jdtls"] = function()
+				-- Set location of JDK
+				local java_home = os.getenv("JAVA_HOME")
+				if java_home == nil then
+					java_home = "/opt/homebrew/opt/openjdk" -- Default Homebrew path for Java
+				end
+
+				-- Set location of jdtls installation from mason
+				local jdtls_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
+				local jdtls_jar = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+				local lombok_jar = vim.fn.glob(jdtls_path .. "/lombok.jar")
+
 				lspconfig["jdtls"].setup({
 					capabilities = capabilities,
 					on_attach = function(client, bufnr)
 						-- Java server-specific settings or event handlers
 					end,
-					-- Additional settings can be specified here according to jdtls documentation
+					cmd = {
+						java_home .. "/bin/java",
+						"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+						"-Dosgi.bundles.defaultStartLevel=4",
+						"-Declipse.product=org.eclipse.jdt.ls.core.product",
+						"-Dlog.protocol=true",
+						"-Dlog.level=ALL",
+						"-javaagent:" .. lombok_jar,
+						"-Xms1g",
+						"--add-modules=ALL-SYSTEM",
+						"--add-opens",
+						"java.base/java.util=ALL-UNNAMED",
+						"--add-opens",
+						"java.base/java.lang=ALL-UNNAMED",
+						"-jar",
+						jdtls_jar,
+						"-configuration",
+						jdtls_path .. "/config_" .. (vim.fn.has("mac") == 1 and "mac" or "linux"),
+						"-data",
+						vim.fn.stdpath("data") .. "/workspace-root/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t"),
+					},
+					root_dir = require("lspconfig").util.root_pattern("pom.xml", "gradle.build", ".git"),
 				})
 			end,
 
